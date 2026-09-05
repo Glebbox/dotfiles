cef1c7dd83a2f97e0701bfda572c33f750977cac
Glebbox <glebcheremnykh@mail.ru>
2026-09-05 14:53:40 +0500

    del

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-DOTFILESDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-echo -e "Download start"
-create_symlink
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -10,3 +10,5 @@ dotpush() {
 }
 # Апдейт
 alias yup='yay -Syu --noconfirm'
+# Вход на ssh-сервер дома
+alias sshconnect='ssh glebboxserver@192.168.1.50'
